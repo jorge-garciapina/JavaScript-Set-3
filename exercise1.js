1372c274fff45242bc359c14b16c947b89398470
jorge-garciapina <115723820+jorge-garciapina@users.noreply.github.com>
2023-03-07 12:14:10 -0600

    Feedback

--- exercise1.js
+++ exercise1.js
@@ -1,33 +1,39 @@
 function viewAllNotes() {
   let notesContainer = document.getElementById("notes-container");
   notesContainer.innerHTML = "";
-  let noteInf = Object.entries(localStorage);
 
-  for (elmnt of noteInf) {
-    appendNoteToHtml(elmnt[0], elmnt[1]);
+  let notesArray = parsedNotesInLocalStorage();
+  if (notesArray.length > 0) {
+    for (let note of notesArray) {
+      let noteInformation = JSON.parse(note);
+      appendNoteToHtml(noteInformation.noteName, noteInformation.noteBody);
+    }
   }
 }
 
 window.addEventListener("load", () => {
-  if (Object.keys(localStorage).length !== 0) {
+  let notesCreated = parsedNotesInLocalStorage();
+  if (notesCreated) {
     viewAllNotes();
+  } else {
+    localStorage.setItem("Notes array", "[]");
   }
 });
 
 // ------------------------------------------
-const inserTab = (() => {
+const tabEventManager = (() => {
   let tabElements = document.querySelectorAll(".tab-able");
   tabElements.forEach((element) => {
     element.addEventListener("keydown", function (event) {
       if (event.key === "Tab") {
         event.preventDefault();
         // Commands to retrieve the position
-        // of the text selection
+        // of the noteBody selection
         const start = element.selectionStart;
         const end = element.selectionEnd;
 
-        // Commands to separate the text before
-        // and after the text selection
+        // Commands to separate the noteBody before
+        // and after the noteBody selection
         const inputText = element.value;
         let textBefore = inputText.substring(0, start);
         let textAfter = inputText.substring(end, inputText.length);
@@ -35,7 +41,7 @@ const inserTab = (() => {
         // Insertion of the tab character:
         element.value = textBefore + "\t" + textAfter;
 
-        // Commands to make the text selection
+        // Commands to make the noteBody selection
         // to behave correctly:
         element.selectionStart = start + 1;
         element.selectionEnd = start + 1;
@@ -46,13 +52,7 @@ const inserTab = (() => {
 
 // ------------------------------------------
 
-// tabElements() is a function is used to generate the
-// note in the HTML. It creates a note with:
-// 1- Title
-// 2- Body
-// 3- Buttons: delete, modify information
-// It also gives functionallity to the buttons
-function appendNoteToHtml(name, text) {
+function appendNoteToHtml(noteName, noteBody) {
   //----------- START: NOTE STYLE----------
 
   let note = document.createElement("template");
@@ -64,73 +64,87 @@ function appendNoteToHtml(name, text) {
   // TITLE:
   let noteTitle = document.createElement("h3");
   noteTitle.className = "new-note-title";
-  noteTitle.textContent = name;
+  noteTitle.textContent = noteName;
 
   // BODY
   let noteText = document.createElement("p");
   noteText.className = "new-note-text";
-  noteText.textContent = text.substring(48);
+  noteText.textContent = noteBody;
   //----------- END: NOTE CONTENT -----------
 
   //----------- START: NOTE BUTTONS----------
   // BUTTON CONTAINER:
-  let noteMenu = document.createElement("div");
+  let notesButtonsContainer = document.createElement("div");
+  notesButtonsContainer.className = "buttonsContainer";
   //--
 
   // DELETE BUTTON:
   let deleteButton = document.createElement("button");
   deleteButton.textContent = "Delete";
+  deleteButton.className = "noteButton";
   // Delete functionality:
   deleteButton.onclick = function () {
     //FEEDBACK:
     if (confirm("Are you sure you want to delete this note?")) {
-      localStorage.removeItem(name);
+      deleteNoteFromLocalStorage(noteName);
       viewAllNotes();
     }
   };
-  deleteButton.style = "margin-right: 5px";
   //--
 
   // MODIFY BUTTON:
   let modifyButton = document.createElement("button");
+  modifyButton.className = "noteButton";
   modifyButton.textContent = "Modify";
   // Modify functionality
   modifyButton.onclick = function () {
-    // FEEDBACK:
-    //-- To make avoid deletion while note is
-    // being modified.
-    noteMenu.removeChild(deleteButton);
+    let buttonCreate = document.getElementById("button-create");
+    buttonCreate.onclick = function () {
+      alert("Finish modifying note");
+    };
+
+    let buttons = Array.from(document.getElementsByClassName("noteButton"));
+    buttons.forEach((element) => {
+      element.style.visibility = "hidden";
+    });
+
     //--
     let title = document.getElementById("modify-title");
     let textElement = document.getElementById("edit-note");
-    title.textContent = name;
-    textElement.value = "";
-    textElement.value = text.substring(48);
-    textElement.textContent = text.substring(48);
+    title.textContent = noteName;
+    textElement.value = noteBody;
   };
   //--
   // INFORMATION BUTTON:
   let informationButton = document.createElement("button");
+  informationButton.className = "noteButton";
   informationButton.textContent = "Info";
-  informationButton.style = "margin-left: 5px";
+  informationButton.className = "informationButton";
   // info functionality
   informationButton.onclick = function () {
-    let rawInfo = localStorage.getItem(name);
-    let creation = rawInfo.substring(0, 24);
-    let lastModified = rawInfo.substring(24, 48);
-    alert("Created: " + creation + "\nLast modification: " + lastModified);
+    for (let note of parsedNotesInLocalStorage()) {
+      if (note.includes(noteName)) {
+        note = JSON.parse(note);
+        alert(
+          "Created: " +
+            note.creationDate +
+            "\nLast modification: " +
+            note.modificationDate
+        );
+      }
+    }
   };
   //--S
-  noteMenu.appendChild(deleteButton);
-  noteMenu.appendChild(modifyButton);
-  noteMenu.appendChild(informationButton);
+  notesButtonsContainer.appendChild(deleteButton);
+  notesButtonsContainer.appendChild(modifyButton);
+  notesButtonsContainer.appendChild(informationButton);
 
   //------------ END: NOTE BUTTONS-----------
 
   // APPENDD ELEMENTS TO THE NOTE:
   note.appendChild(noteTitle);
   note.appendChild(noteText);
-  note.appendChild(noteMenu);
+  note.appendChild(notesButtonsContainer);
   //--
 
   // APPEND NOTE TO THE CONTAINER IN DOM:
@@ -141,42 +155,105 @@ function appendNoteToHtml(name, text) {
 // ------------------------------------------
 // createNewNote() is used to define the title and body
 // of a note, and then append to the HTML
-let i = 1;
 function createNewNote() {
   let noteBody = document.getElementById("new-note");
   let noteName = document.getElementById("note-name");
-  let date = new Date();
+  let isTheNameEmpty = (() => noteName.value === "")();
 
-  date = String(date).substring(0, 24);
-  if (noteName.value !== "" && noteBody.value !== "") {
-    appendNoteToHtml(noteName.value, date + date + noteBody.value);
-    localStorage.setItem(noteName.value, date + date + noteBody.value);
+  if (isTheNameEmpty) {
+    alert("Choose a title");
+  } else if (isTheNameTaken(noteName.value)) {
+    alert("Name already taken. Chose another name");
+  } else {
+    let date = new Date();
+    date = String(date).substring(0, 24);
+
+    let noteInformation = {
+      noteName: noteName.value,
+      creationDate: date,
+      modificationDate: date,
+      noteBody: noteBody.value,
+    };
+    let noteInJsonFormat = JSON.stringify(noteInformation);
+
+    appendNoteToHtml(noteInformation.noteName, noteInformation.noteBody);
+
+    let notesStoredInLocalStorage = parsedNotesInLocalStorage();
+    notesStoredInLocalStorage.push(noteInJsonFormat);
+    updateLocalStorage(notesStoredInLocalStorage);
+
     noteBody.value = "";
     noteName.value = "";
-  } else {
-    alert("Missing information");
   }
+}
 
-  i++;
+// ------------------------------------------
+function isTheNameTaken(noteName) {
+  if (noteName === "") {
+    return 0;
+  }
+  let notesStoredInLocalStorage = parsedNotesInLocalStorage();
+
+  for (let note of notesStoredInLocalStorage) {
+    if (note.includes(noteName)) {
+      return true;
+    }
+  }
+  return false;
+}
+
+// ------------------------------------------
+function deleteNoteFromLocalStorage(noteName) {
+  let notesStoredInLocalStorage = parsedNotesInLocalStorage();
+
+  for (let note of notesStoredInLocalStorage) {
+    if (note.includes(noteName)) {
+      let indexOfNoteToDelete = notesStoredInLocalStorage.indexOf(note);
+      notesStoredInLocalStorage.splice(indexOfNoteToDelete, 1);
+      updateLocalStorage(notesStoredInLocalStorage);
+    }
+  }
+}
+
+// ------------------------------------------
+function updateLocalStorage(notesArray) {
+  notesArray = JSON.stringify(notesArray);
+  localStorage.setItem("Notes array", notesArray);
+}
+// ------------------------------------------
+function parsedNotesInLocalStorage() {
+  return JSON.parse(localStorage.getItem("Notes array"));
 }
 // ------------------------------------------
 
-// saveChanges() is used to save the changes when user modify a note
 function saveChanges() {
-  if (Object.keys(localStorage).length > 0) {
+  let buttonCreate = document.getElementById("button-create");
+  buttonCreate.onclick = function () {
+    createNewNote();
+  };
+  let notesStoredInLocalStorage = parsedNotesInLocalStorage();
+  if (notesStoredInLocalStorage.length > 0) {
     let title = document.getElementById("modify-title");
     let textElement = document.getElementById("edit-note");
-    let content = localStorage.getItem(title.textContent);
-    let creationDate = content.substring(0, 24);
-    let modificationDate = new Date();
-    modificationDate = String(modificationDate).substring(0, 24);
+    let newNotesArray = [];
 
-    localStorage.setItem(
-      title.textContent,
-      creationDate + modificationDate + textElement.value
-    );
+    for (let note of notesStoredInLocalStorage) {
+      if (note.includes(title.textContent)) {
+        note = JSON.parse(note);
+        note.noteBody = textElement.value;
+        let date = new Date();
+        note.modificationDate = String(date).substring(0, 24);
+        note = JSON.stringify(note);
+        newNotesArray.push(note);
+      } else {
+        newNotesArray.push(note);
+      }
+    }
+
+    updateLocalStorage(newNotesArray);
+    viewAllNotes();
+
     title.textContent = "Note to modify";
     textElement.value = "";
   }
-  viewAllNotes();
 }
